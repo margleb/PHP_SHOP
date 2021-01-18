1c9759b14dd989dfd069bcc217ed6ede954510d7
Gleb Martianov <margleb@yandex.ru>
2021-01-18 15:19:31 +0300

    Создание IndexController для административной части

---
+++ core/admin/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace core\admin\controller;
+use core\base\controllers\BaseController;
+
+class IndexController extends BaseController
+{
+    protected function inputData() {
+        exit('I am admin panel');
+    }
+
+}
