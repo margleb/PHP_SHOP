fa38b270f260d81fd7ba9934e989f9eb76eb6230
Gleb Martianov <margleb@yandex.ru>
2021-01-22 08:49:14 +0300

    Теория Msql связи один к многим и многие к многим

--- core/admin/controllers/IndexController.php
+++ core/admin/controllers/IndexController.php
@@ -11,7 +11,22 @@ class IndexController extends BaseController
 
         $db = Model::instance();
 
-        $query = "SELECT name1 FROM articles";
+        /* ОДИН КО МНОГИМ */
+
+        # вложенный запрос
+        // $query = "SELECT id, name FROM product WHERE parent_id = ( SELECT id FROM category WHERE name='Apple')";
+
+        # с помочью одной инструкции LeftJoin
+        // $query = "SELECT product.id, product.name FROM product LEFT JOIN category ON product.parent_id = category.id WHERE category.id = 1";
+        // $query = "SELECT category.id, category.name, product.id as p_id, product.name as p_name FROM product LEFT JOIN category ON product.parent_id = category.id";
+
+        /* МНОГИЕ КО МНОГИМ */
+        $query = "SELECT teachers.id, teachers.name, students.id as s_id, students.name as s_name 
+        FROM teachers 
+        LEFT JOIN stud_teach ON teachers.id = stud_teach.teachers
+        LEFT JOIN students ON stud_teach.students = students.id
+        ";
+
         $res = $db->query($query);
 
         exit('I am admin panel');
