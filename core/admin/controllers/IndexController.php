518b36004434ed062051358fe040acc11d16a458
Gleb Martianov <margleb@yandex.ru>
2021-01-22 11:12:10 +0300

    создание методов createFields, createOrder

--- core/admin/controllers/IndexController.php
+++ core/admin/controllers/IndexController.php
@@ -13,15 +13,15 @@ class IndexController extends BaseController
 
         $table = 'teachers';
 
-//        $res = $db->get($table, [
-//            'fields' => ['id', 'name'],
-//            'where' => ['fio' => 'smirnova', 'name' => 'Masha', 'surname' => 'Sergeeva'],
-//            'operand' => ['=', '<>'], # <> не равно
-//            'condition' => ['AND'],
-//            'order' => ['fio', 'name'],
-//            'order_direction' => ['ASC', 'DESC'],
-//            'limit' => '1'
-//        ]);
+        $res = $db->get($table, [
+            'fields' => ['id', 'name'],
+            'where' => ['fio' => 'smirnova', 'name' => 'Masha', 'surname' => 'Sergeeva'],
+            'operand' => ['=', '<>'], # <> не равно
+            'condition' => ['AND'],
+            'order' => ['fio', 'name'],
+            // 'order_direction' => ['DESC'],
+            'limit' => '1'
+        ]);
 
 
         exit('I am admin panel');
