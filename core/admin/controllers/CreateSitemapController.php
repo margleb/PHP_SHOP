5b6f8a3e8eb16e8dc9b275e0a5620f2f87c16ecd
Gleb Martianov <margleb@yandex.ru>
2021-04-04 16:54:59 +0300

    Парсер сайтов часть 2

--- core/admin/controllers/CreateSitemapController.php
+++ core/admin/controllers/CreateSitemapController.php
@@ -11,7 +11,7 @@ namespace core\admin\controllers;
 
 use core\base\controllers\BaseMethods;
 
-class CreateSitemapController
+class CreateSitemapController extends BaseAdmin
 {
     use BaseMethods;
 
@@ -54,7 +54,70 @@ class CreateSitemapController
 
     }
 
-    protected function parsing($usl, $index = 0) {
+    protected function parsing($url, $index = 0) {
+        // если в конце указан слеш, то прекращаем выполнение
+        // для того чтобы в site map не появлялось 2 ссылки с конечным слешем и без него
+        if(mb_strlen(SITE_URL) + 1 == mb_strlen($url) &&
+            mb_strrpos($url, '/') == mb_strlen($url) -1) return;
+
+        $curl = curl_init();
+        // куда отправлять запросы (url)
+        curl_setopt($curl, CURLOPT_URL, $url);
+        // нужны ли ответы с сервера
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        // возращать ли заголовки
+        curl_setopt($curl, CURLOPT_HEADER, true);
+        // следовать ли curl за редиректами
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        // ожидание ответа сервера
+        curl_setopt($curl, CURLOPT_TIMEOUT, 120);
+        // ограничием обьем данных для загрузки curl
+        curl_setopt($curl, CURLOPT_RANGE, 0 - 4194304);
+
+        // инициализируем curl
+        $out = curl_exec($curl);
+
+        // останавливаем curl
+        curl_close($curl);
+
+        // echo('CURL SUCCESS');
+        // exit($out);
+
+        // разбираем заголовки
+        // s - многострочный поиск
+
+        // ищем только html страницы
+        if(!preg_match("/Content-Type:\s+text\/html/uis", $out)) {
+
+
+            // разрегестрируем ссылку по которой пришли
+            unset($this->linkArr[$index]);
+
+            // выставляем заново нумерацию ключей
+            $this->linkArr = array_values($this->linkArr);
+
+            return;
+        }
+
+
+        // проверяем что код ответа от 200 и более
+        if(!preg_match('/HTTP\/\d.?\d?\s+20\d/uis', $out)) {
+            $this->writeLog('Не корректная ссылка при парсине - ' . $url, $this->parsingLogFile);
+
+            // разрегестрируем ссылку по которой пришли
+            unset($this->linkArr[$index]);
+
+            // выставляем заново нумерацию ключей
+            $this->linkArr = array_values($this->linkArr);
+
+            $_SESSION['res']['answer'] = '<div class="error">Incorrect link in parsing - '. $url . '<br>Sitemap is created</div>';
+
+            return;
+        } else {
+            exit('yes');
+        }
+
+
 
     }
 
