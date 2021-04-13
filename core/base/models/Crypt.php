561fb02d959627b9922c0089a2cc5a8dfdb50b48
Gleb Martianov <margleb@yandex.ru>
2021-04-13 08:46:16 +0300

    модернизация системы шифрования

--- core/base/models/Crypt.php
+++ core/base/models/Crypt.php
@@ -29,7 +29,17 @@ class Crypt
         // хеш ключ
         $hmac = hash_hmac($this->hashAlgoritm, $cipherText, CRYPT_KEY, true);
 
-        return base64_encode($iv.$hmac.$cipherText);
+        // return base64_encode($iv.$hmac.$cipherText);
+
+        $cipherText_comb = '11223344556677';
+        $iv_comb = 'abcdefg';
+        $hmac_comb = '000000000000';
+
+        // перемещиваем
+        // $res = '1122a33b445c5000000000000667d78899efg';
+
+        $res = $this->cryptCombine($cipherText_comb, $iv_comb, $hmac_comb);
+
     }
 
     public function decrypt($str) {
@@ -51,6 +61,43 @@ class Crypt
         if(hash_equals($hmac, $calcmac)) return $original_plaintext;
 
         return false;
+    }
+
+    protected function cryptCombine($str, $iv, $hmac) {
+
+        $new_str = '';
+
+        $str_len = strlen($str);
+
+        $counter = (int)ceil(strlen(CRYPT_KEY) / ($str_len + strlen($hmac)));
+
+        $progress = 1;
+
+        if($counter >= $str_len) $counter = 1;
+
+        for($i = 0; $i < $str_len; $i++) {
+            if($counter < $str_len) {
+                if($counter === $i) {
+                    $new_str .= substr($iv, $progress - 1, 1);
+                    $progress++;
+                    $counter += $progress;
+                }
+            } else {
+                break;
+            }
+
+            $new_str .= substr($str, $i, 1);
+        }
+
+        $new_str .= substr($str, $i);
+        $new_str .= substr($iv, $progress - 1);
+
+        $new_str_half = (int)ceil(strlen($new_str) / 2);
+
+        $new_str = substr($new_str, 0, $new_str_half) . $hmac . substr($new_str, $new_str_half);
+
+        return base64_encode($new_str);
+
     }
 
 }
