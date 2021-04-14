e4bfea2b7ad606a9d408df1a2838334ebe44ef08
Gleb Martianov <margleb@yandex.ru>
2021-04-14 10:54:46 +0300

    дешифровка данных

--- core/base/models/Crypt.php
+++ core/base/models/Crypt.php
@@ -29,36 +29,26 @@ class Crypt
         // хеш ключ
         $hmac = hash_hmac($this->hashAlgoritm, $cipherText, CRYPT_KEY, true);
 
-        // return base64_encode($iv.$hmac.$cipherText);
+        return $this->cryptCombine($cipherText, $iv, $hmac);
 
-        $cipherText_comb = '11223344556677';
-        $iv_comb = 'abcdefg';
-        $hmac_comb = '000000000000';
-
-        // перемещиваем
-        // $res = '1122a33b445c5000000000000667d78899efg';
-
-        $res = $this->cryptCombine($cipherText_comb, $iv_comb, $hmac_comb);
+        $crypt_data = $this->cryptUncombine($res, $ivlen);
 
     }
 
     public function decrypt($str) {
-        // декодируем
-        $crypt_str = base64_decode($str);
+
+
         // получаем длину вектора шифрования
         $ivlen = openssl_cipher_iv_length($this->cryptMethod);
-        // из шифрованной строки вытаскиваем первые 16 символов
-        $iv = substr($crypt_str, 0, $ivlen);
-        // получаем hash ключ
-        $hmac = substr($crypt_str, $ivlen, $this->hashLength);
-        // получаем конечную шифруемую строку
-        $cipherText = substr($crypt_str, $ivlen + $this->hashLength);
+
+        $crypt_data = $this->cryptUncombine($str, $ivlen);
+
         // дешифруем
-        $original_plaintext = openssl_decrypt($cipherText, $this->cryptMethod, CRYPT_KEY, OPENSSL_RAW_DATA, $iv);
+        $original_plaintext = openssl_decrypt($crypt_data['str'], $this->cryptMethod, CRYPT_KEY, OPENSSL_RAW_DATA, $crypt_data['iv']);
 
-        $calcmac = hash_hmac($this->hashAlgoritm, $cipherText, CRYPT_KEY, true);
+        $calcmac = hash_hmac($this->hashAlgoritm, $crypt_data['str'], CRYPT_KEY, true);
 
-        if(hash_equals($hmac, $calcmac)) return $original_plaintext;
+        if(hash_equals($crypt_data['hmac'], $calcmac)) return $original_plaintext;
 
         return false;
     }
@@ -69,7 +59,8 @@ class Crypt
 
         $str_len = strlen($str);
 
-        $counter = (int)ceil(strlen(CRYPT_KEY) / ($str_len + strlen($hmac)));
+        // 1. Вычесляем позицию с которой будем вставляеть вектор шифрования
+        $counter = (int)ceil(strlen(CRYPT_KEY) / ($str_len + $this->hashLength));
 
         $progress = 1;
 
@@ -78,26 +69,80 @@ class Crypt
         for($i = 0; $i < $str_len; $i++) {
             if($counter < $str_len) {
                 if($counter === $i) {
+                    // 3. Начиная с вычесленной позиции записываем букву из вектора шифрования
                     $new_str .= substr($iv, $progress - 1, 1);
                     $progress++;
+                    // 4. Увеличиваем позицию через определенное количество букв
                     $counter += $progress;
                 }
             } else {
+                // 5. Если вычесленна позиция больше строки то останавливам цикл
                 break;
             }
-
+            // 2. Заносим в новую строку
             $new_str .= substr($str, $i, 1);
         }
 
+        // 6. Добавляем в конец оставшуюся строку
         $new_str .= substr($str, $i);
+        // 7. А также оставшиеся символы с вектора шифрования
         $new_str .= substr($iv, $progress - 1);
 
+        // 7. Добавляем в середину хеш
         $new_str_half = (int)ceil(strlen($new_str) / 2);
-
         $new_str = substr($new_str, 0, $new_str_half) . $hmac . substr($new_str, $new_str_half);
 
         return base64_encode($new_str);
 
     }
+
+    protected function cryptUncombine($str, $ivlen) {
+
+        $crypt_data = [];
+        $str = base64_decode($str);
+
+        // 1. вычленяем хеш
+        $hash_position = (int)ceil(strlen($str) / 2 - $this->hashLength / 2);
+        $crypt_data['hmac'] = substr($str, $hash_position, $this->hashLength);
+        $str = str_replace($crypt_data['hmac'], '', $str);
+
+        // 2. Вычесляем позицию вектора шифрования
+        $counter = (int)ceil(strlen(CRYPT_KEY) / (strlen($str) - $ivlen + $this->hashLength));
+
+        $progerss = 2;
+
+        $crypt_data['str'] = '';
+        $crypt_data['iv'] = '';
+
+        for($i = 0; $i < strlen($str); $i++) {
+
+            // 3. Проверяем добавляли ли мы в конец строки вектор шифроания и оставшуюся строку
+            if($ivlen + strlen($crypt_data['str']) < strlen($str)) {
+
+                // 4. Раскидываем на вектор шифрования и символ
+                if($i == $counter) {
+                    $crypt_data['iv'] .= substr($str, $counter, 1);
+                    $progerss++;
+                    $counter += $progerss;
+                } else {
+                    $crypt_data['str'] .= substr($str, $i, 1);
+                }
+
+            } else {
+
+                $crypt_data_len = strlen($crypt_data['str']);
+
+                $crypt_data['str'] .= substr($str, $i, strlen($str) - $ivlen - $crypt_data_len);
+                $crypt_data['iv'] .= substr($str, $i + (strlen($str) - $ivlen - $crypt_data_len));
+                break;
+
+            }
+        }
+
+        return $crypt_data;
+
+    }
+
+
 
 }
--- core/user/controllers/IndexController.php
+++ core/user/controllers/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends BaseController {
 
     protected function inputData() {
 
-        $str = '1234567890';
+        $str = '1234567890abcde';
 
         $en_str = Crypt::instance()->encrypt($str);
 
